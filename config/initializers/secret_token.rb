7dcd4d244d9b14b7fe3e8019cec4e14fa21330ae
ohmatt <jmatt306@gmail.com>
2013-04-25 20:50:26 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LspAlpha::Application.config.secret_token = '92304c910a1cc86d7d0a17dc6a44c2242458ec2d37fd12b46579d6896b96010a7e512033c5e0739a3b57badf441d4fa98e943f17374f3888b6b1ed70a7bb73b4'
