929f23fa31226b2cd443eee19050b701d4203e31
Matt Johnson <jmatt306@gmail.com>
2013-05-09 17:07:09 -0400

    Set up default images for buyers, vendors, and the user Profile Image. Made changes to path images are stored at.

--- app/models/bus_buyer.rb
+++ app/models/bus_buyer.rb
@@ -3,12 +3,14 @@ class BusBuyer < ActiveRecord::Base
 	belongs_to :user
 
 	# This method associates the attribute ":logo" with a file attachment
-	has_attached_file :logo, styles: {
-		smthumb: '50x50#',
-		thumb: '100x100>',
-		square: '200x200#',
-		medium: '300x300>'},
-		:default_style => :square
+	has_attached_file :logo, 
+		styles: {
+			smthumb: '50x50>',
+			thumb: '100x100>',
+			medium: '300x300#'},
+		:default_style => :thumb,
+		:default_url => '/images/default/:attachment_:style.png',
+		:path => 'buyers/:id/images/logos/:basename_:style.:extension'
 
 	validates_presence_of :busName, :on => :create, :message => "You must provide a valid business to use LandSpec!"
 end
--- app/models/bus_vendor.rb
+++ app/models/bus_vendor.rb
@@ -6,12 +6,14 @@ class BusVendor < ActiveRecord::Base
 	accepts_nested_attributes_for :locations
 
 	# This method associates the attribute ":logo" with a file attachment
-	has_attached_file :logo, styles: {
-		smthumb: '50x50>',
-		thumb: '100x100>',
-		square: '200x200#',
-		medium: '300x300>'},
-		:default_style => :square
+	has_attached_file :logo, 
+		styles: {
+			smthumb: '50x50>',
+			thumb: '100x100>',
+			medium: '300x300#'},
+		:default_style => :thumb,
+		:default_url => '/images/default/:attachment_:style.png',
+		:path => 'vendors/:id/images/logos/:basename_:style.:extension'
 
 	validates_presence_of :busName, :on => :create, :message => "You must provide a valid business to use LandSpec!"
 end
