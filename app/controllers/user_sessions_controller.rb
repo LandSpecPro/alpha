2d188e31bf1c4d88d9ffbe702dbbace099308c57
ohmatt <jmatt306@gmail.com>
2013-06-14 00:23:15 -0400

    More changes for landing page.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -12,7 +12,7 @@ class UserSessionsController < ApplicationController
     if @user_session.save
       flash[:notice] = "Login successful!"
       if UserSession.find.user.is_vendor
-        redirect_to business_vendor_dashboard_url
+        redirect_to locations_manage_url
       elsif UserSession.find.user.is_buyer
         redirect_to business_buyer_dashboard_url
       else
