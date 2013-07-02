ef3b13bc7444b47888c6c16e387a5a4efe5a7fbb
ohmatt <jmatt306@gmail.com>
2013-07-02 13:56:26 -0400

    Updated sizes for Buyer and Supplier logos so they are not cropped the same.

--- app/models/bus_buyer.rb
+++ app/models/bus_buyer.rb
@@ -11,8 +11,8 @@ class BusBuyer < ActiveRecord::Base
 	has_attached_file :logo, 
 		styles: {
 			smthumb: '50x50>',
-			thumb: '100x100#',
-			medium: '300x300#'},
+			thumb: '100x100>',
+			medium: '300x300>'},
 		:default_style => :thumb,
 		:default_url => '/images/default/:attachment_:style.png',
 		:path => 'buyers/:id/images/logos/:basename_:style.:extension'
