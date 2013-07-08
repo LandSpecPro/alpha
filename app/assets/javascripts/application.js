ac1fdf5d1476fd38f0dec760bfad782784d22ad9
Matt Johnson <jmatt306@gmail.com>
2013-07-08 19:50:38 -0400

    Updated theme assets for Ace Admin.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require bootstrap
-//= require theme/jquery-ui-1.10.2.custom.min
+//= require theme/jquery-ui-1.10.3.custom.min
 //= require theme/jquery.ui.touch-punch.min
 //= require theme/chosen.jquery.min
 //= require theme/jquery.gritter.min
