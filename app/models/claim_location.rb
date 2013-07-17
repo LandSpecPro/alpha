4819593d43f090d6137201f2064c2c87ec94a3b6
Matt Johnson <jmatt306@gmail.com>
2013-07-17 12:28:51 -0400

    Added geolocation to ClaimLocations. Requires manual rake command to update all coordinates in database.

--- app/models/claim_location.rb
+++ app/models/claim_location.rb
@@ -1,3 +1,16 @@
 class ClaimLocation < ActiveRecord::Base
-  attr_accessible :claimed
+  	attr_accessible :claimed
+
+  	geocoded_by :get_full_address
+  	after_validation :geocode
+
+  	def get_full_address
+		@address1 = self.loc_address1 + " "
+		@address2 = self.loc_address2 + " "
+		@city = self.loc_city + ", "
+		@state = self.loc_state + " "
+		@zip = self.loc_zip
+
+		return @address1 + @address2 + @city + @state + @zip
+	end
 end
