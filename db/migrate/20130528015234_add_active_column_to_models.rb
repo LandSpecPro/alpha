b70f045b5f575c029cb4847358e1323f98123f78
ohmatt <jmatt306@gmail.com>
2013-07-27 18:45:50 -0400

    Removed a bunch of category files so I can start over from scratch.

--- db/migrate/20130528015234_add_active_column_to_models.rb
+++ db/migrate/20130528015234_add_active_column_to_models.rb
@@ -3,15 +3,12 @@ class AddActiveColumnToModels < ActiveRecord::Migration
   	add_column :users, :active, :boolean, :default => true, :null => false
   	add_column :bus_buyers, :active, :boolean, :default => true, :null => false
   	add_column :bus_vendors, :active, :boolean, :default => true, :null => false
-  	add_column :categories, :active, :boolean, :default => true, :null => false
   	add_column :contacts, :active, :boolean, :default => true, :null => false
   	add_column :fav_locations, :active, :boolean, :default => true, :null => false
   	add_column :fav_products, :active, :boolean, :default => true, :null => false
   	add_column :featured_items, :active, :boolean, :default => true, :null => false
   	add_column :locations, :active, :boolean, :default => true, :null => false
-  	add_column :location_has_categories, :active, :boolean, :default => true, :null => false
   	add_column :products, :active, :boolean, :default => true, :null => false
-  	add_column :product_has_categories, :active, :boolean, :default => true, :null => false
   	add_column :product_images, :active, :boolean, :default => true, :null => false
   	add_column :search_logs, :active, :boolean, :default => true, :null => false
   end
@@ -20,15 +17,12 @@ class AddActiveColumnToModels < ActiveRecord::Migration
   	remove_column :users, :active
   	remove_column :bus_buyers, :active
   	remove_column :bus_vendors, :active
-  	remove_column :categories, :active
   	remove_column :contacts, :active
   	remove_column :fav_locations, :active
   	remove_column :fav_products, :active
   	remove_column :featured_items, :active
   	remove_column :locations, :active
-  	remove_column :location_has_categories, :active
   	remove_column :products, :active
-  	remove_column :product_has_categories, :active
   	remove_column :product_images, :active
   	remove_column :search_logs, :active
   end
