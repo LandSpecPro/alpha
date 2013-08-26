0fc3c9e6bff0e5c559ad8956375f9e4ca8c3d13a
Matt Johnson <jmatt306@gmail.com>
2013-08-26 10:31:19 -0400

    Added a few more plants to the database seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,10 @@ Category.create([
 	{ id: 117, parent_id: 1, hierarchy_level: 2, cat_name: 'Wetland Plans'},
 	{ id: 118, parent_id: 1, hierarchy_level: 2, cat_name: 'Vegetables'},
 	{ id: 119, parent_id: 1, hierarchy_level: 2, cat_name: 'Bulbs'},
-	{ id: 120, parent_id: 1, hierarchy_level: 2, cat_name: 'Cacti & Succulents'}
+	{ id: 120, parent_id: 1, hierarchy_level: 2, cat_name: 'Cacti & Succulents'},
+	{ id: 121, parent_id: 1, hierarchy_level: 2, cat_name: 'Balled and Burlapped'},
+	{ id: 122, parent_id: 1, hierarchy_level: 2, cat_name: 'Plant Liners'},
+	{ id: 123, parent_id: 1, hierarchy_level: 2, cat_name: 'Container Stock'}
 #
 #	#MATERIALS
 #	{ id: 200, parent_id: 2, hierarchy_level: 2, cat_name: 'Block'},
