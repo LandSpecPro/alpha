5f6656e7b096b9565bb77a4aab3fd32cd190c640
Matt Johnson <jmatt306@gmail.com>
2013-09-21 13:22:44 -0400

    Last payment push before removing all payments for now.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,26 +5,26 @@ class PaymentsController < ApplicationController
 
 	def notify
 
-    notify = Paypal::Notification.new(request.raw_post)
+	    notify = Paypal::Notification.new(request.raw_post)
 
-    location = Location.find(notify.invoice)
+	    location = Location.find(notify.invoice)
 
-    if notify.acknowledge
-        if notify.complete?
-          @location = Location.find(notify.invoice)
-          @location.is_subscribed_to_inventory = true
-          @location.save
-        else
-          logger.error("Failed to verify Paypal's notification, please investigate")
-        end
-    else
-    	logger.error("Paypal didn't acknowledge!")
-    	# TODO - Need to send an error to me in case anything goes wrong here. That way I can check into it as soon as it happens
-    	# because we don't want users paying and then getting nothing.
-    end
+	    if notify.acknowledge
+	        if notify.complete?
+	          @location = Location.find(notify.invoice)
+	          @location.is_subscribed_to_inventory = true
+	          @location.save
+	        else
+	          logger.error("Failed to verify Paypal's notification, please investigate")
+	        end
+	    else
+	    	logger.error("Paypal didn't acknowledge!")
+	    	# TODO - Need to send an error to me in case anything goes wrong here. That way I can check into it as soon as it happens
+	    	# because we don't want users paying and then getting nothing.
+	    end
 
-    render :nothing => true
+	    render :nothing => true
 
-  end
+	end
 
 end
