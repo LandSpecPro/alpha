3ce8804243c5ac17bba84aad3a264e96b5c3677e
Matt Johnson <jmatt306@gmail.com>
2013-09-28 22:48:45 -0400

    Fixed merge with database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20130928235512) do
-=======
-ActiveRecord::Schema.define(:version => 20130922001448) do
->>>>>>> master
 
   create_table "bus_buyers", :force => true do |t|
     t.string   "busName",                              :null => false
