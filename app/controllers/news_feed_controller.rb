287091502b3a1d121dac8f44c99b87440cfaddc9
Matt Johnson <jmatt306@gmail.com>
2013-10-08 22:08:09 -0400

    Limited news feed posts, changed some text.

--- app/controllers/news_feed_controller.rb
+++ app/controllers/news_feed_controller.rb
@@ -6,16 +6,28 @@ class NewsFeedController < ApplicationController
 
 		store_location
 
-		if params[:only_featured_items]
-			@items = NewsFeedItem.where(:item_type => NEW_FEATURED_ITEM).order('created_at DESC')
-		elsif params[:only_new_suppliers]
-			@items = NewsFeedItem.where(:item_type => NEW_SUPPLIER).order('created_at DESC')
-		elsif params[:only_locations]
-			@items = NewsFeedItem.where(:item_type => NEW_LOCATION).order('created_at DESC')
-		elsif params[:only_inventories]
-			@items = NewsFeedItem.where("item_type = ? or item_type = ?", FIRST_INVENTORY, NEW_INVENTORY).order('created_at DESC')
+		if params[:offset]
+			offset = params[:offset]
 		else
-			@items = NewsFeedItem.order('created_at DESC')
+			offset = 0
+		end
+
+		if params[:limit]
+			limit = params[:limit]
+		else
+			limit = 50
+		end
+
+		if params[:only_featured_items]
+			@items = NewsFeedItem.where(:item_type => NEW_FEATURED_ITEM).order('created_at DESC').offset(offset).limit(limit)
+		elsif params[:only_new_suppliers]
+			@items = NewsFeedItem.where(:item_type => NEW_SUPPLIER).order('created_at DESC').offset(offset).limit(limit)
+		elsif params[:only_locations]
+			@items = NewsFeedItem.where(:item_type => NEW_LOCATION).order('created_at DESC').offset(offset).limit(limit)
+		elsif params[:only_inventories]
+			@items = NewsFeedItem.where("item_type = ? or item_type = ?", FIRST_INVENTORY, NEW_INVENTORY).order('created_at DESC').offset(offset).limit(limit)
+		else
+			@items = NewsFeedItem.order('created_at DESC').offset(offset).limit(limit)
 		end
 
 	end
