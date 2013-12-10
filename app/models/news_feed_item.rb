0f87ed0ff5205a688ab70641058a96194d0cfddd
Matt Johnson <jmatt306@gmail.com>
2013-12-09 19:51:20 -0500

    Added in links on the side of the news feed to see who you're following.

--- app/models/news_feed_item.rb
+++ app/models/news_feed_item.rb
@@ -2,4 +2,8 @@ class NewsFeedItem < ActiveRecord::Base
 
 	attr_accessible :item_type, :item_id, :user_id, :location_id, :created_at
 
+	def self.only_following(user_id)
+		return NewsFeedItem.joins('INNER JOIN follows ON follows.location_id = news_feed_items.location_id WHERE follows.active = true AND follows.user_id = ' + user_id.to_s)
+	end
+
 end
