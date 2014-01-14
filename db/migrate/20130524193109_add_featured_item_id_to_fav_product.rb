524cc7e61f5fc8f94d40bb40567db2e92299dc59
ohmatt <jmatt306@gmail.com>
2014-01-14 18:32:16 -0500

    Removed other migrations that were breaking a new database install.

--- db/migrate/20130524193109_add_featured_item_id_to_fav_product.rb
+++
@@ -1,9 +0,0 @@
-class AddFeaturedItemIdToFavProduct < ActiveRecord::Migration
-  def up
-  	add_column :fav_products, :featured_item_id, :integer
-  end
-
-  def down
-  	remove_column :fav_products, :featured_item_id
-  end
-end
