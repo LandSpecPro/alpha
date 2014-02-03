969aa27f04d2daf8755704b2ac76d7c0a9c9ef2b
ohmatt <jmatt306@gmail.com>
2014-02-03 11:28:12 -0500

    Fixed bug where new version of paperclip requires specifying content type to upload inventory.

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -8,4 +8,6 @@ class Inventory < ActiveRecord::Base
 
   validates_presence_of :file, :message => "No inventory file selected!"
 
+  validates_attachment_content_type :file, :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'application/pdf', 'application/msword', 'application/vnd.ms-word', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/msexcel', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/mspowerpoint', 'application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'text/plain']
+
 end
